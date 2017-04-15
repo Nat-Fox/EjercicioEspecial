8b82ad83beb05cd04841d11d189f44f8120eb027
Natalia Garrido <nat@MacBook-Air-de-Maria.local>
2017-04-15 18:55:01 -0300

    enunciado ejercicio

---
+++ ejercicioEspecial.js
@@ -0,0 +1,7 @@
+/**
+ * Pedir al usuario 2 números y que elija el orden de las operaciones que desea que se realicen con esos
+ * números, las opciones son:
+ * Suma, resta, multiplicación y división
+ * 
+ * Dependiendo de la elección del orden del usuario mostrar los resultados
+ */
